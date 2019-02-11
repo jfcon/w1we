7908c139712ab75f71d8563ce4f8a7597b006114
John <connollyjohnf@gmail.com>
2019-02-11 03:36:42 +0000

    end function comes up last

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
-console.log('I did it!');
+// console.log('I did it!');
+
+var options = {
+    host: 'www.example.com',
+    path: '/'
+}
+
+// called by https when the request is made
+function callback(){
+    console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
